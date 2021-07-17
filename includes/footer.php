0c3b78a3ec9ccfe6f0ae26d59e29df395445ff1c
Sumit das <sumitkumardas090@gmail.com>
2021-07-17 18:00:17 +0530

    main

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy 2021 Shreemit Advance Blood Care Unit</p>
+        </div>
+        <!-- /.container -->
+    </footer>
